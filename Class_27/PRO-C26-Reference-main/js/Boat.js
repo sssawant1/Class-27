9844bf361fc984b6c73d78b9b7f172b092ce237c
sssawant1 <87284685+sssawant1@users.noreply.github.com>
2022-04-11 17:53:07 -0400

    Add files via upload

---
+++ Class_27/PRO-C26-Reference-main/js/Boat.js
@@ -0,0 +1,45 @@
+class Boat {
+  constructor(x, y, width, height, boatPos, boatAnimation) {
+    this.animation =boatAnimation;
+    this.speed =0.05;
+
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+    this.boatPosition = boatPos;
+    //this.image = loadImage("./assets/boat.png"); <=now we are animating
+    this.isBroken =false;
+    World.add(world, this.body);
+  }
+
+  animate(){
+    this.speed +=0.05;
+
+  }
+
+  remove(index) {
+    this.animation =brokenBoatAnimation;
+    this.speed =0.05;
+    this.width =300;
+    this.height =300;
+    this.isBroken =true;
+    setTimeout(() => {
+      Matter.World.remove(world, boats[index].body);
+      delete boats[index];
+    }, 2000);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    var index =floor(this.speed % this.animation.length)
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
+
